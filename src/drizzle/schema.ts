c643d091524bacf15739060d7e8dc4fe334e9eed
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-08-20 08:10:25 +0800

    feat: Add drizzle ORM and schema definitions for user and token tables

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,37 @@
+import {
+  boolean,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum("role", ["user", "admin"]);
+
+export const user = pgTable("user", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  profileImage: text("profile_image").notNull(),
+  alertNotification: boolean("alert_notification").default(true).notNull(),
+  role: roleEnum("role").default("user").notNull(),
+  createdAt: timestamp("created_at")
+    .notNull()
+    .$defaultFn(() => new Date())
+    .notNull(),
+});
+
+export const token = pgTable("token", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  type: text("type").notNull(),
+  token: text("token").notNull(),
+  profileImage: text("profile_image").notNull(),
+  alertNotification: boolean("alert_notification").default(true).notNull(),
+  role: roleEnum("role").default("user").notNull(),
+  expiredAt: timestamp("expires_at"),
+  createdAt: timestamp("created_at")
+    .notNull()
+    .$defaultFn(() => new Date())
+    .notNull(),
+});
