d35e35383d12393a2e948670e868c49919812987
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-08-22 04:15:17 +0800

    refactor: Rename usersController to userController

---
+++ src/controllers/userController.ts
@@ -0,0 +1,83 @@
+import { config } from "dotenv";
+import { Request, Response } from "express";
+
+import { db } from "../drizzle";
+import { verifyToken } from "../lib/auth";
+import { user } from "../drizzle/schema";
+import { schoolEmailSchema } from "../lib/schema";
+import { getUserByEmailAndOauthId } from "../lib/service/user-service";
+
+config({ path: ".env.local" });
+
+export async function createUser(req: Request, res: Response) {
+  try {
+    const { name, email, profileImage, oauthId, tokenType } = req.body;
+
+    const missingFields = [];
+    if (!name) missingFields.push("name");
+    if (!email) missingFields.push("email");
+    if (!profileImage) missingFields.push("profileImage");
+    if (!oauthId) missingFields.push("oauthId");
+    if (!tokenType) missingFields.push("tokenType");
+
+    if (missingFields.length > 0) {
+      return res.status(400).send({
+        message: `Missing required fields: ${missingFields.join(", ")}`,
+        error: "Bad Request",
+        statusCode: 400,
+      });
+    }
+
+    const isValidEmail = schoolEmailSchema.safeParse(email);
+
+    if (isValidEmail.error) {
+      return res.status(400).send({
+        message: isValidEmail.error.message,
+        error: "Bad Request",
+        statusCode: 400,
+      });
+    }
+
+    const isValidToken = await verifyToken(req, tokenType);
+
+    if (!isValidToken?.isValidToken) {
+      return res.status(401).send({
+        message: "Invalid or expired authentication token",
+        error: "Unauthorized",
+        statusCode: 401,
+      });
+    }
+
+    const userExist = await getUserByEmailAndOauthId(email, oauthId);
+
+    if (userExist) {
+      return res.status(409).send({
+        message: "User with this email and OAuth ID already exists",
+        error: "Conflict",
+        statusCode: 409,
+      });
+    }
+
+    const newUserValues = {
+      name,
+      email,
+      oauthId,
+      profileImage,
+    };
+
+    const [newUser] = await db.insert(user).values(newUserValues).returning();
+
+    return res.status(201).send({
+      message: "User created successfully",
+      statusCode: 201,
+      data: newUser,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message:
+        "An unexpected error occurred while creating the account. Please try again later. If the problem persists, contact support.",
+      error: "Internal Server Error",
+      statusCode: 500,
+    });
+  }
+}
