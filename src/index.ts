67a95e9220878794dd80a423e64c0f6ac95fdfa1
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-10-10 04:49:38 +0800

    feat: Integrate @hono/node-server and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 
-const app = new Hono()
+const app = new Hono();
 
-const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
+app.get("/", (c) => c.text("Hello Node.js!"));
 
-app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
-
-export default app
+serve(app);
