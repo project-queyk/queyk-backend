e58da81b7be5b7cc567f86da2b5fe9e4fd377799
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-09-11 03:46:22 +0800

    chore: make token type unique

--- internal/adapters/postgresql/migrations/00002_tokens.sql
+++ internal/adapters/postgresql/migrations/00002_tokens.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS public."token" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    type text NOT NULL,
+    type text NOT NULL UNIQUE,
     token text NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
     expires_at TIMESTAMP WITH TIME ZONE
